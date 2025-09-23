0d98f5f2064259b98666c440e854218c2e5b56cd
Akbar Jorayev <100585930+akbarjorayev@users.noreply.github.com>
2025-09-23 19:23:16 +0500

    Use optional chaining for caption in getListCallbackCaption

--- utils/list.js
+++ utils/list.js
@@ -1,5 +1,5 @@
 function getListCallbackCaption(data) {
-  const caption = data.caption || ''
+  const caption = data?.caption || ''
   const googleMaps = `[Google Maps](https://www.google.com/maps?q=${data?.location.latitude},${data?.location.longitude})`
   const yandexMaps = `[Yandex Maps](https://yandex.com/maps/?pt=${data?.location.longitude},${data?.location.latitude}&z=18&l=map)`
   const createdAt = `*Created at:* ${new Date(
